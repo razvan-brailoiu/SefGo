8af1a46ea113220ee6ec6283282d4b86f519269c
razb <razvan.brailoiu@student.upt.ro>
2022-05-17 16:42:09 +0300

    Commit after SEF-11

--- src/main/java/org/loose/fis/sre/controllers/AdminInitController.java
+++ src/main/java/org/loose/fis/sre/controllers/AdminInitController.java
@@ -1,4 +1,32 @@
 package org.loose.fis.sre.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AdminInitController {
+
+    @FXML
+    private Button addNewEmployeeButton;
+
+    private Parent root;
+
+    @FXML
+    public void iniateAddNewEmployee(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/register.fxml"));
+            root = (Parent) loader.load();
+            Stage window = (Stage) addNewEmployeeButton.getScene().getWindow();
+            window.setTitle("InApp");
+            window.setScene(new Scene(root, 634, 428));
+
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/main/java/org/loose/fis/sre/controllers/DevInitController.java
+++ src/main/java/org/loose/fis/sre/controllers/DevInitController.java
@@ -1,4 +1,40 @@
 package org.loose.fis.sre.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DevInitController {
+
+    Parent root;
+
+    @FXML
+    private Button logOutButton;
+
+    @FXML
+    public void iniateLogOut(){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+        try {
+            root = (Parent) loader.load();
+            Stage window = (Stage) logOutButton.getScene().getWindow();
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Logout");
+            alert.setHeaderText("You are about to log out of your account!");
+            alert.setContentText("Leave for sure ? ");
+            if(alert.showAndWait().get() == ButtonType.OK) {
+                window.setTitle("Login");
+                window.setScene(new Scene(root, 634, 428));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
